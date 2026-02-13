127e3b63a754d7cd9206d68a69645c116062bcab
Atul Kumar Singh <atulsingh.0369@gmail.com>
2026-02-14 01:29:23 +0530

    feat: Update `EXEC_SECRET` and add `LINEAR_API_KEY` to ecosystem configuration.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,9 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3001,
-        EXEC_SECRET: "atul_singh_linear_369"
-      }
-    }
-  ]
+        EXEC_SECRET: "GcUqWhAo2QP1RDwkQKcK2wlisa98Q0OS",
+        LINEAR_API_KEY: "lin_api_7blho0sEGW5nnXlEkHXh6x1YWMcWMYHY7VzN2RTs",
+      },
+    },
+  ],
 };
